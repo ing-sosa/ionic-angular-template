2d6b4c240a7178d0eefe8fd4ca4e2b9bf20eeaf6
Jose Carlos LB <Joslos_carlos@hotmail.com>
2025-07-17 16:32:50 -0600

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,25 +5,19 @@ export const routes: Routes = [
   {
     path: 'pdf-template',
     loadComponent: () =>
-      import('./pages/welcome/welcome.page').then(m => m.WelcomePage),
-    children: [
-      {
-        path: '', // cuando entras a /pdf-template muestra lista de plantillas
-        loadComponent: () =>
-          import('./pages/pdf-templates/pdf-templates.component').then(m => m.PdfTemplatesComponent),
-      },
-      {
-        path: 'crear', // /pdf-template/create para crear nueva plantilla
-        loadComponent: () =>
-          import('./pages/pdf-templates/components/template-edit/template-edit.component').then(m => m.TemplateEditComponent),
-      },
-      {
-        path: 'editar/:id', // /pdf-template/edit/123 para editar plantilla
-        loadComponent: () =>
-          import('./pages/pdf-templates/components/template-edit/template-edit.component').then(m => m.TemplateEditComponent),
-      },
-    ],
+      import('./pages/pdf-templates/pdf-templates.component').then(m => m.PdfTemplatesComponent),
   },
+  {
+    path: 'pdf-template/crear',
+    loadComponent: () =>
+      import('./pages/pdf-templates/components/template-edit/template-edit.component').then(m => m.TemplateEditComponent),
+  },
+  {
+    path: 'pdf-template/editar/:id',
+    loadComponent: () =>
+      import('./pages/pdf-templates/components/template-edit/template-edit.component').then(m => m.TemplateEditComponent),
+  },
+
   {
     path: '**',
     redirectTo: 'pdf-template',

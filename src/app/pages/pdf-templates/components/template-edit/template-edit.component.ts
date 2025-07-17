a34b2753304e7f96478d0e110fa47e4f511bfa9c
Jose Carlos LB <Joslos_carlos@hotmail.com>
2025-07-17 03:08:32 -0600

    Obtención de datos

--- src/app/pages/pdf-templates/components/template-edit/template-edit.component.ts
+++ src/app/pages/pdf-templates/components/template-edit/template-edit.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { IonSelectOption, IonSelect, IonCardHeader, IonCardTitle, IonButton, IonIcon, IonCardContent, IonCard, IonItem, IonLabel, IonRange, IonSegmentButton, IonSegment, IonContent, IonToolbar, IonGrid, IonRow, IonCol, IonToggle, IonInput, IonCheckbox, IonRadioGroup, IonRadio, IonImg, IonTitle, IonHeader, IonButtons, IonFooter } from "@ionic/angular/standalone";
 
@@ -32,7 +32,8 @@ import { IonSelectOption, IonSelect, IonCardHeader, IonCardTitle, IonButton, Ion
     IonCardTitle,
     IonCardHeader,
     CommonModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class TemplateEditComponent implements OnInit {
@@ -40,14 +41,82 @@ export class TemplateEditComponent implements OnInit {
   useImage: boolean = false;
   watermark: boolean = false;
 
-  constructor(private route: ActivatedRoute) { }
+  templateForm!: FormGroup;
+
+
+  constructor(private route: ActivatedRoute, private fb: FormBuilder) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.templateId = params.get('id') ?? ''
     })
 
-    console.log('CREAR PLANTILLA');
+    this.templateForm = this.fb.group({
+      name: [''],
+      pageSize: ['carta'],
+      header: this.fb.array([
+        this.createColumn()
+      ]),
+      footer: this.fb.array([
+        this.createColumn()
+      ]),
+      watermark: this.fb.group({
+        enabled: [false],
+        opacity: [0.5],
+        width: [200],
+        height: [200],
+        image: [''] // base64 o path
+      })
+    });
+  }
+
+  createColumn(): FormGroup {
+    return this.fb.group({
+      type: ['text'], // text o image
+      content: [''],
+      styles: this.fb.group({
+        bold: [false],
+        italic: [false],
+        bullet: [false],
+        color: ['#000000'],
+        width: [100], // solo si es imagen
+        height: [100]
+      })
+    });
+  }
+
+  get headerColumns(): FormArray {
+    return this.templateForm.get('header') as FormArray;
+  }
+
+  get footerColumns(): FormArray {
+    return this.templateForm.get('footer') as FormArray;
+  }
+
+  get watermarkGroup(): FormGroup {
+    return this.templateForm.get('watermark') as FormGroup;
+  }
+
+
+  addHeaderColumn() {
+    this.headerColumns.push(this.createColumn());
+  }
+
+  removeHeaderColumn(index: number) {
+    this.headerColumns.removeAt(index);
+  }
+
+  addFooterColumn() {
+    this.footerColumns.push(this.createColumn());
+  }
+
+  removeFooterColumn(index: number) {
+    this.footerColumns.removeAt(index);
+  }
+
+  // Útil para debug:
+  logForm() {
+    console.log(this.templateForm.value);
   }
 
   pinOpacity(value: number) {

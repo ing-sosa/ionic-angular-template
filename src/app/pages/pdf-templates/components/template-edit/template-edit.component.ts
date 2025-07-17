2baee5c9bf421d9055ce1b09cf7276a421ccae7a
Jose Carlos LB <Joslos_carlos@hotmail.com>
2025-07-16 23:48:13 -0600

    Avance crear plantilla

--- src/app/pages/pdf-templates/components/template-edit/template-edit.component.ts
+++ src/app/pages/pdf-templates/components/template-edit/template-edit.component.ts
@@ -1,22 +1,43 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { IonCardHeader, IonCardTitle, IonButton, IonIcon } from "@ionic/angular/standalone";
+import { IonSelectOption, IonSelect, IonCardHeader, IonCardTitle, IonButton, IonIcon, IonCardContent, IonCard, IonItem, IonLabel, IonRange, IonSegmentButton, IonSegment, IonContent, IonToolbar, IonGrid, IonRow, IonCol, IonToggle, IonInput, IonCheckbox, IonRadioGroup, IonRadio } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-template-edit',
   templateUrl: './template-edit.component.html',
   styleUrls: ['./template-edit.component.scss'],
   standalone: true,
-  imports: [
+  imports: [IonRadio, IonRadioGroup, IonCheckbox,
+    IonInput,
+    IonToggle,
+    IonCol,
+    IonRow,
+    IonGrid,
+    IonToolbar,
+    IonContent,
+    IonSegment,
+    IonSegmentButton,
+    IonRange,
+    IonLabel,
+    IonSelectOption,
+    IonItem,
+    IonCard,
+    IonSelect,
+    IonCardContent,
     RouterLink,
-    IonIcon, IonButton, IonCardTitle, IonCardHeader,
-    CommonModule
+    IonIcon,
+    IonButton,
+    IonCardTitle,
+    IonCardHeader,
+    CommonModule,
+    FormsModule
   ]
 })
 export class TemplateEditComponent implements OnInit {
-
   templateId: string = '';
+  useImage: boolean = false;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -28,4 +49,7 @@ export class TemplateEditComponent implements OnInit {
     console.log('CREAR PLANTILLA');
   }
 
+  onToggleChange(ev: any) {
+    console.log('Nuevo valor:', ev.detail.value);
+  }
 }
